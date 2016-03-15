a56d8718f96a303265e3b411aeeb0d8e92a5835e
xiaoyuzdy <1104503035@qq.com>
2016-03-15 18:18:50 +0800

    code
    安卓学习代码

---
+++ FristAndroid/app/src/main/java/com/example/he/fristandroid/FristActivity.java
@@ -0,0 +1,15 @@
+package com.example.he.fristandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by he on 2016/3/15.
+ */
+public class FristActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fristlayout);//加载布局
+    }
+}
