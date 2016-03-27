db80afee1d0daf14d6e9f1ecc331ebe99937c3a7
xiaoyuzdy <1104503035@qq.com>
2016-03-27 22:11:15 +0800

    16/3/27

--- FristLineCode/FirstAndroid3/app/src/main/java/com/example/he/firstandroid3/MainActivity.java
+++ FristLineCode/FirstAndroid3/app/src/main/java/com/example/he/firstandroid3/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.he.firstandroid3;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+/**
+ *基本控件的使用
+ */
+
 public class MainActivity extends AppCompatActivity {
-    private Button button,button2;
+    private Button button, button2, button3, button4,button5;
     private EditText editText;
     private ImageView imageView;
-    private int count=1;//用于统计点击次数 实现换图片
+    private ProgressBar progressBar;
+    private int count = 1;//用于统计点击次数 实现换图片
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,29 +31,84 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.first_layout);
         button = (Button) findViewById(R.id.button1);
-        button2=(Button) findViewById(R.id.button2);
-        editText=(EditText)findViewById(R.id.edit_text);
-        imageView=(ImageView)findViewById(R.id.image_view);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4=(Button) findViewById(R.id.button4);
+        button5=(Button) findViewById(R.id.button5);
+
+        editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String data=editText.getText().toString();
-                Toast.makeText(MainActivity.this,data,Toast.LENGTH_SHORT).show();
+                String data = editText.getText().toString();
+                Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
             }
         });
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (count%2==1){
+                if (count % 2 == 1) {
                     imageView.setImageResource(R.drawable.ic_launcher);
-                }else{
+                } else {
                     imageView.setImageResource(R.drawable.second);
                 }
                 count++;
             }
         });
 
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //如果进度条消失加载进度条
+                if (progressBar.getVisibility() == View.GONE) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress((int) (progressBar.getProgress() + Math.random() * 10));//模拟进度条加载的情况
+
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                }
+
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("这是一个警告框");
+                dialog.setMessage("是否确定退出程序");
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //
+                        finish();
+                    }
+                });
+                dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //
+                    }
+                });
+                dialog.show();
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressDialog dialog=new ProgressDialog(MainActivity.this);
+                dialog.setTitle("this is ProgressDialog");
+                dialog.setMessage("请耐心等待");
+                dialog.setCancelable(true);
+                dialog.show();
+            }
+        });
     }
 }
