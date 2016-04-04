3f86fea04e343f3cf522aa6e9ca840843eaf7398
xiaoyuzdy <1104503035@qq.com>
2016-04-04 17:22:37 +0800

    16/4/4

--- FristLineCode/FragmentTest/app/src/main/java/com/example/he/fragmenttest/RightFragment.java
+++ FristLineCode/FragmentTest/app/src/main/java/com/example/he/fragmenttest/RightFragment.java
@@ -2,7 +2,6 @@ package com.example.he.fragmenttest;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +10,14 @@ import android.view.ViewGroup;
  * Created by he on 2016/4/3.
  */
 public class RightFragment extends Fragment {
-    @Nullable
-    @Override
+
+//    在碎片中得到当前碎片相关联的活动的实例
+    mainActivity ac = (com.example.he.fragmenttest.mainActivity) getActivity();
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.right_fragment,container,false);
-        return  view;
+        View view = inflater.inflate(R.layout.right_fragment, container, false);
+        //通过活动获取另一个碎片的实例
+//        FragmentManager manager= ac.getFragmentManager();
+//        LeftFragment fragment= (LeftFragment) manager.findFragmentById(R.id.left_fragment);
+        return view;
     }
 }
--- FristLineCode/FragmentTest/app/src/main/java/com/example/he/fragmenttest/mainActivity.java
+++ FristLineCode/FragmentTest/app/src/main/java/com/example/he/fragmenttest/mainActivity.java
@@ -1,15 +1,55 @@
 package com.example.he.fragmenttest;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by he on 2016/4/3.
  */
+//public class mainActivity extends Activity implements View.OnClickListener {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main_layout);
+//        Button button = (Button) findViewById(R.id.button);
+//        button.setOnClickListener(this);
+//    }
+//
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.button:
+//                AnotherRightFragment fragment = new AnotherRightFragment();
+//                FragmentManager manager = getFragmentManager();
+//                FragmentTransaction transaction = manager.beginTransaction();
+//                transaction.replace(R.id.right_layout, fragment);
+//                transaction.commit();
+//                break;
+//        }
+//    }
+//}
 public class mainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        Button button = (Button) findViewById(R.id.button);
+//        从布局文件中获取碎片实例
+//        RightFragment rightFragment= (RightFragment) getFragmentManager().findFragmentById(R.id.right_fragment);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AnotherRightFragment fragment = new AnotherRightFragment();//碎片实例
+                FragmentManager manager = getFragmentManager();//得到FragmentManager
+                FragmentTransaction transaction = manager.beginTransaction();//开启事务
+                transaction.replace(R.id.right_layout, fragment);//替换碎片
+                transaction.addToBackStack(null);//将事务添加到返回栈
+                transaction.commit();//提交
+            }
+        });
     }
 }
