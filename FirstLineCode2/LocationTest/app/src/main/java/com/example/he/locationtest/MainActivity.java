ebf890aace35dcb3756073e0f2b9a4e5a87a2e0a
zcczdy <1104503035@qq.com>
2016-05-07 18:03:16 +0800

    16/5/7

--- FirstLineCode2/LocationTest/app/src/main/java/com/example/he/locationtest/MainActivity.java
+++ FirstLineCode2/LocationTest/app/src/main/java/com/example/he/locationtest/MainActivity.java
@@ -7,16 +7,27 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 public class MainActivity extends Activity {
     private TextView position;
     private String provider;
     private LocationManager manager;
+    public static final int SHOW_LOCATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +60,7 @@ public class MainActivity extends Activity {
         if (location != null) {
             showLocation(location);
         }
-        manager.requestLocationUpdates(provider,5000,1,locationListener);
+        manager.requestLocationUpdates(provider, 5000, 1, locationListener);
 
     }
 
@@ -93,9 +104,63 @@ public class MainActivity extends Activity {
         }
     };
 
-    private void showLocation(Location location) {
-        String p = "latitude is " + location.getLatitude() + "\n" + "longitude is " + location.getLongitude();
-        position.setText(p);
+    private void showLocation(final Location location) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    StringBuilder u = new StringBuilder();
+                    u.append("http://maps.googleapis.com/maps/api/geocode/json?latlng=");
+                    u.append(location.getLatitude()).append(",");
+                    u.append(location.getLongitude());
+                    u.append("&sensor=false");
+
+                    //获取返回数据
+                    URL url = new URL(u.toString());
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setRequestProperty("Accept-Language", "zh-CN");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream input = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+
+                    //解析JSON
+                    JSONObject jsonObject = new JSONObject(response.toString());
+                    //获取results节点的位置信息
+                    JSONArray resultArray = jsonObject.getJSONArray("results");
+                    if (resultArray.length() > 0) {
+                        JSONObject subObject = resultArray.getJSONObject(0);
+                        String address = subObject.getString("formatted_address");
+                        Message message = new Message();
+                        message.what = SHOW_LOCATION;
+                        message.obj = address;
+                        handler.sendMessage(message);
+
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SHOW_LOCATION:
+                    position.setText((String) msg.obj);
+                    break;
+            }
+        }
+    };
 
 }
