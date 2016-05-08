b499deaea1dfa117f5078a3106b6ca3e105e3e92
zcczdy <1104503035@qq.com>
2016-05-08 17:08:10 +0800

    16/5/8

---
+++ FirstLineCode2/Baidumaptest/app/src/main/java/com/example/he/baidumaptest/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.he.baidumaptest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.model.LatLng;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mapView;
+    private BaiduMap baiduMap;
+    private LocationManager manager;
+    private String probider;
+    private boolean isFirstLocate = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());//初始化操作
+        setContentView(R.layout.main_layout);
+        mapView = (MapView) findViewById(R.id.map_view);
+
+        baiduMap = mapView.getMap();
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        List<String> list = manager.getProviders(true);
+        if (list.contains(LocationManager.GPS_PROVIDER)) {
+            probider = LocationManager.GPS_PROVIDER;
+        } else if (list.contains(LocationManager.NETWORK_PROVIDER)) {
+            probider = LocationManager.NETWORK_PROVIDER;
+        } else {
+            Toast.makeText(this, "no find location provider", Toast.LENGTH_SHORT).show();
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = manager.getLastKnownLocation(probider);
+        if (location != null) {
+            navigateTo(location);
+        }
+
+        manager.requestLocationUpdates(probider, 5000, 1, locationListener);
+
+    }
+
+    //在地图中显示位置
+    private void navigateTo(Location location) {
+        if (isFirstLocate) {
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(latLng);
+            baiduMap.animateMapStatus(update);
+            update = MapStatusUpdateFactory.zoomTo(16f);//设置缩放级别
+            baiduMap.animateMapStatus(update);
+            isFirstLocate = false;
+        }
+    }
+
+
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            navigateTo(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+        if (manager != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            manager.removeUpdates(locationListener);
+        }
+    }
+}
