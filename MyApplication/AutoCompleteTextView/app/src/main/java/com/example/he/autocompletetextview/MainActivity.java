ff157ff166e65f0e89e19933b9b1c6e49e8a4e08
zcczdy <1104503035@qq.com>
2016-10-23 21:37:06 +0800

    16/10/23

--- MyApplication/AutoCompleteTextView/app/src/main/java/com/example/he/autocompletetextview/MainActivity.java
+++ MyApplication/AutoCompleteTextView/app/src/main/java/com/example/he/autocompletetextview/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.CompoundButton;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,13 +20,15 @@ public class MainActivity extends AppCompatActivity {
     private MultiAutoCompleteTextView textView2;
     private ArrayAdapter<String> adapter;
     private List<String> list;
+    private ToggleButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (AutoCompleteTextView) findViewById(R.id.t1);
-        textView2= (MultiAutoCompleteTextView) findViewById(R.id.t2);
+        textView2 = (MultiAutoCompleteTextView) findViewById(R.id.t2);
+        button = (ToggleButton) findViewById(R.id.tb1);
         list = new ArrayList<String>();
         list.add("shanghai1");
         list.add("shanghai2");
@@ -32,5 +37,14 @@ public class MainActivity extends AppCompatActivity {
         textView.setAdapter(adapter);
         textView2.setAdapter(adapter);
         textView2.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+
+        button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //isChecked?
+                Toast.makeText(MainActivity.this, isChecked ? "开" : "关", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
