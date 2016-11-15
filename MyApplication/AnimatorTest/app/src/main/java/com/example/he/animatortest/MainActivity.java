1e4567b7477cb9091b7de914d6baa63a90da5620
zcczdy <1104503035@qq.com>
2016-11-15 22:12:42 +0800

    11/15

--- MyApplication/AnimatorTest/app/src/main/java/com/example/he/animatortest/MainActivity.java
+++ MyApplication/AnimatorTest/app/src/main/java/com/example/he/animatortest/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.he.animatortest;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final float N = 120F;//每次弹出，收回菜单每个图标移动的距离
 
     private ImageView imageView;
     private Button button;
+
+    private int res[] = {R.id.imageView_a, R.id.imageView_b, R.id.imageView_c, R.id.imageView_d, R.id.imageView_e, R.id.imageView_f, R.id.imageView_g, R.id.imageView_h};//图片ID数组
+    private List<ImageView> imageViewList;
+
+    private float anim_Y;//图片的起始Y坐标
+    private boolean flag = false;//false表示菜单第一次按下，则弹出菜单，true表示第二次按下，则收回菜单
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +37,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         imageView.setOnClickListener(this);
         button.setOnClickListener(this);
 
+        imageViewList = new ArrayList<ImageView>();
+
+        for (int i = 0; i < res.length; i++) {
+            ImageView anim_image = (ImageView) findViewById(res[i]);
+            anim_Y = anim_image.getY();
+            anim_image.setOnClickListener(this);
+            imageViewList.add(anim_image);
+
+
+        }
+
 
     }
+
+    //在XML中配置了onClick属性
+    public void click(View view) {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(view, "alpha", 0F, 1F);
+        animator.setDuration(1000);
+        animator.start();
+        //给动画添加监听事件
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                Toast.makeText(MainActivity.this, "动画执行完毕", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+    }
+
 
     @Override
     public void onClick(View v) {
@@ -54,10 +110,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                  * 使用AnimatorSet，为imageView添加动画集合，效果和上面一样,增强了对属性动画的控制
                  */
 
-                ObjectAnimator animator1= ObjectAnimator.ofFloat(imageView,"translationX",0F,100F);
-                ObjectAnimator animator2= ObjectAnimator.ofFloat(imageView,"rotation",0F,360F);
-                ObjectAnimator animator3=ObjectAnimator.ofFloat(imageView,"translationY",0,100);
-                AnimatorSet set=new AnimatorSet();
+                ObjectAnimator animator1 = ObjectAnimator.ofFloat(imageView, "translationX", 0F, 100F);
+                ObjectAnimator animator2 = ObjectAnimator.ofFloat(imageView, "rotation", 0F, 360F);
+                ObjectAnimator animator3 = ObjectAnimator.ofFloat(imageView, "translationY", 0, 100);
+                AnimatorSet set = new AnimatorSet();
 //                set.playTogether(animator1,animator2);//添加动画效果,同时进行
 //                set.playSequentially(animator1,animator2);//添加动画效果,相继进行
                 set.play(animator1).with(animator2);//右移加旋转
@@ -66,6 +122,77 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 set.start();
 
                 break;
+
+            //展开动画
+            case R.id.imageView_a:
+                if (!flag)
+                    startAnim();
+                else
+                    closeAnim();
+                break;
+            case R.id.imageView_b:
+                break;
+            case R.id.imageView_c:
+                break;
+            case R.id.imageView_d:
+                break;
+            case R.id.imageView_e:
+                break;
+            case R.id.imageView_f:
+                break;
+            case R.id.imageView_g:
+                break;
+
+
         }
     }
+
+    /**
+     * 展开菜单
+     */
+    private void startAnim() {
+        ObjectAnimator animator = null;
+        for (int i = 1; i < res.length; i++) {
+            animator = ObjectAnimator.ofFloat(imageViewList.get(i), "translationY", anim_Y, -N * i);
+            animator.setDuration(1000);
+            animator.setInterpolator(new BounceInterpolator());//添加插值器，小球弹动效果
+            animator.start();
+        }
+
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                //弹出动画结束后将flag变为true
+                flag=true;
+                super.onAnimationEnd(animation);
+            }
+        });
+
+    }
+
+
+    /**
+     * 收回菜单
+     */
+
+    private void closeAnim() {
+        ObjectAnimator animator = null;
+        for (int i = 1; i < res.length; i++) {
+            animator = ObjectAnimator.ofFloat(imageViewList.get(i), "translationY",  -N * i,anim_Y);
+            animator.setDuration(1000);
+            animator.setStartDelay(i*300);
+            animator.start();
+        }
+
+        animator.addListener(new AnimatorListenerAdapter() {
+            //弹出动画结束后将flag变为true
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                flag = false;
+                super.onAnimationEnd(animation);
+            }
+        });
+    }
+
+
 }
