374d5b30dbb46090ebd3fb8d4f3e0e28045cf034
zcczdy <1104503035@qq.com>
2016-12-20 17:52:13 +0800

    12/20

--- View/myViewPager/app/src/main/java/com/example/he/myviewpager/MainActivity.java
+++ View/myViewPager/app/src/main/java/com/example/he/myviewpager/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.he.myviewpager;
 
 import android.content.Intent;
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        TextView textView= (TextView) findViewById(R.id.ttt);
+        TransitionDrawable drawable= (TransitionDrawable) textView.getBackground();
+        drawable.startTransition(3000);
+
+        ImageView imageView= (ImageView) findViewById(R.id.clip_image);
+        ClipDrawable clip= (ClipDrawable) imageView.getBackground();
+        clip.setLevel(8000);//设置等级（0～10000），表示剩余80%,裁剪了20%
+
+
     }
 }
