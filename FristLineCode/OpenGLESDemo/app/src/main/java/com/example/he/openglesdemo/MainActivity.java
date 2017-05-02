0cb21466ef5a4982707065a156acfca1134ad916
zcczdy <1104503035@qq.com>
2017-05-02 15:52:55 +0800

    commit

--- FristLineCode/OpenGLESDemo/app/src/main/java/com/example/he/openglesdemo/MainActivity.java
+++ FristLineCode/OpenGLESDemo/app/src/main/java/com/example/he/openglesdemo/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.he.openglesdemo;
 import android.content.Context;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -34,6 +37,17 @@ public class MainActivity extends AppCompatActivity {
 class MyGLSurfaceView extends GLSurfaceView {
 
     private Renderer mRenderer;
+    //投影矩阵
+    // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
+
+
+    //触摸监听
+    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+    private float mPreviousX;
+    private float mPreviousY;
 
 
     public MyGLSurfaceView(Context context) {
@@ -45,29 +59,71 @@ class MyGLSurfaceView extends GLSurfaceView {
 
         //如果选用这一配置选项，那么除非调用了requestRender()，否则GLSurfaceView不会被重新绘制，这样做可以让应用的性能及效率得到提高。
 
-        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);//仅在你的绘制数据发生变化时才在视图中进行绘制操作
+//        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);//仅在你的绘制数据发生变化时才在视图中进行绘制操作
     }
 
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
     //自定义渲染类,执行与绘制相关的工作
     private class MyRenderer implements Renderer {
+
+        private Triangle mTriangle;
+        private Square   mSquare;
+        private float[] mRotationMatrix = new float[16];
+
 
         @Override
         public void onSurfaceCreated(GL10 gl, EGLConfig config) {
             // Set the background frame color
             GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+            mTriangle=new Triangle();
+            mSquare=new Square();
+
         }
 
         //如果View的几何形态发生变化时会被调用，例如当设备的屏幕方向发生改变时。
         @Override
         public void onSurfaceChanged(GL10 gl, int width, int height) {
             gl.glViewport(0, 0, width, height);
+
+            float ratio = (float) width / height;
+            // this projection matrix is applied to object coordinates
+            // in the onDrawFrame() method
+            Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
         }
 
         //每次重绘View时调用
         @Override
         public void onDrawFrame(GL10 gl) {
             gl.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+            float[] scratch = new float[16];
+
+            // Set the camera position (View matrix)
+            Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+            // Calculate the projection and view transformation
+            Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+            // Draw shape
+//            mTriangle.draw(mMVPMatrix);
+
+            // Create a rotation transformation for the triangle
+            long time = SystemClock.uptimeMillis() % 4000L;
+            float angle = 0.090f * ((int) time);
+            Matrix.setRotateM(mRotationMatrix, 0, angle, 0, 0, -1.0f);
+
+            // Combine the rotation matrix with the projection and camera view
+            // Note that the mMVPMatrix factor *must be first* in order
+            // for the matrix multiplication product to be correct.
+            Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+
+            // Draw triangle
+            mTriangle.draw(scratch);
 
         }
     }
--- FristLineCode/OpenGLESDemo/app/src/main/java/com/example/he/openglesdemo/Triangle.java
+++ FristLineCode/OpenGLESDemo/app/src/main/java/com/example/he/openglesdemo/Triangle.java
@@ -1,5 +1,7 @@
 package com.example.he.openglesdemo;
 
+import android.opengl.GLES20;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -8,15 +10,63 @@ import java.nio.FloatBuffer;
  * OpenGL ES允许我们使用三维空间的坐标来定义绘画对象。所以在我们能画三角形之前，必须先定义它的坐标。
  * <p>
  * 定义一个三角形
+ * 这个形状的坐标是以逆时针顺序定义的
  * Created by he on 2017/5/2.
  */
 
 public class Triangle {
 
+    /**
+     * 顶点着色器（Vertex Shader）：用来渲染形状顶点的OpenGL ES代码。
+     */
+    private final String vertexShaderCode =
+            // This matrix member variable provides a hook to manipulate
+            // the coordinates of the objects that use this vertex shader
+            "uniform mat4 uMVPMatrix;" +
+                    "attribute vec4 vPosition;" +
+                    "void main() {" +
+                    // the matrix must be included as a modifier of gl_Position
+                    // Note that the uMVPMatrix factor *must be first* in order
+                    // for the matrix multiplication product to be correct.
+                    "  gl_Position = uMVPMatrix * vPosition;" +
+                    "}";
+
+    /**
+     * 片段着色器（Fragment Shader）：使用颜色或纹理渲染形状表面的OpenGL ES代码。
+     */
+    private final String fragmentShaderCode =
+            "precision mediump float;" +
+                    "uniform vec4 vColor;" +
+                    "void main() {" +
+                    "  gl_FragColor = vColor;" +
+                    "}";
+
+
+    // Use to access and set the view transformation
+    private int mMVPMatrixHandle;
+
+
     private FloatBuffer vertexBuffer;
+
+    private final int mProgram;
+
 
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
+
+
+    private int mPositionHandle;
+    private int mColorHandle;
+
+    private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+
+
+    /**
+     * 在这个坐标系中，[0, 0, 0]（分别对应X轴坐标, Y轴坐标, Z轴坐标）对应的是GLSurfaceView的中心。
+     * 逆序定义
+     */
+
     static float triangleCoords[] = {   // in counterclockwise order:
             0.0f, 0.622008459f, 0.0f, // top
             -0.5f, -0.311004243f, 0.0f, // bottom left
@@ -39,6 +89,86 @@ public class Triangle {
         vertexBuffer.put(triangleCoords);
         // set the buffer to read the first coordinate
         vertexBuffer.position(0);
+
+
+        int vertexShader = MyGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER,
+                vertexShaderCode);
+        int fragmentShader = MyGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER,
+                fragmentShaderCode);
+
+        // create empty OpenGL ES Program
+        mProgram = GLES20.glCreateProgram();
+
+        // add the vertex shader to program
+        GLES20.glAttachShader(mProgram, vertexShader);
+
+        // add the fragment shader to program
+        GLES20.glAttachShader(mProgram, fragmentShader);
+
+        // creates OpenGL ES program executables
+        GLES20.glLinkProgram(mProgram);
+
+
     }
+
+
+    public void draw(float[] mvpMatrix) {
+        // Add program to OpenGL ES environment
+        GLES20.glUseProgram(mProgram);
+
+        // get handle to vertex shader's vPosition member
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
+        // Enable a handle to the triangle vertices
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        // Prepare the triangle coordinate data
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT, false,
+                vertexStride, vertexBuffer);
+
+        // get handle to fragment shader's vColor member
+        mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+
+        // Set color for drawing the triangle
+        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+
+        // Draw the triangle
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+
+        // Disable vertex array
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+
+
+        // get handle to shape's transformation matrix
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+
+        // Pass the projection and view transformation to the shader
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
+
+        // Draw the triangle
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+
+        // Disable vertex array
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+
+
+    }
+
+
+    static class MyGLRenderer {
+        public static int loadShader(int type, String shaderCode) {
+            // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+            // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+            int shader = GLES20.glCreateShader(type);
+
+            // add the source code to the shader and compile it
+            GLES20.glShaderSource(shader, shaderCode);
+            GLES20.glCompileShader(shader);
+
+            return shader;
+        }
+    }
+
 
 }
