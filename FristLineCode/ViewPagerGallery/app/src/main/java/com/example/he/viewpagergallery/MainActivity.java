cbd281592cc643da7e1cfdabed9c666d732d6181
zcczdy <1104503035@qq.com>
2017-05-02 18:57:51 +0800

    commit

--- FristLineCode/ViewPagerGallery/app/src/main/java/com/example/he/viewpagergallery/MainActivity.java
+++ FristLineCode/ViewPagerGallery/app/src/main/java/com/example/he/viewpagergallery/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.he.viewpagergallery;
 
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 利用ViewPager打造画廊效果
@@ -10,6 +15,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager  mViewPager;
+    private List<View> viewList;
 
 
     @Override
@@ -17,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mViewPager= (ViewPager) findViewById(R.id.view_pager);
+        viewList=new ArrayList<>();
+
+        ImageView im1=new ImageView(this);
+
+//        im1.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+
+
 
 
     }
