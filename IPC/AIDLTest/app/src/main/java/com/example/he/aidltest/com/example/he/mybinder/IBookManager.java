75d82786d1a23079df5dbf1d902fc6e207269fa6
zcczdy <1104503035@qq.com>
2017-05-08 19:25:45 +0800

    commit

--- IPC/AIDLTest/app/src/main/java/com/example/he/aidltest/com/example/he/mybinder/IBookManager.java
+++ IPC/AIDLTest/app/src/main/java/com/example/he/aidltest/com/example/he/mybinder/IBookManager.java
@@ -3,6 +3,7 @@ package com.example.he.aidltest.com.example.he.mybinder;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.IInterface;
+import android.os.Parcel;
 import android.os.RemoteException;
 
 import com.example.he.aidltest.Book;
@@ -30,26 +31,97 @@ public interface IBookManager extends IInterface {
     public class BookManagerImpl extends Binder implements IBookManager {
 
 
-        public BookManagerImpl(){
+        public BookManagerImpl() {
             //连接接口
-            this.attachInterface(this,DESCRIPTOR);
+            this.attachInterface(this, DESCRIPTOR);
         }
 
-        
+        public static IBookManager asInterface(IBinder obj) {
+            if (obj == null)
+                return null;
+            android.os.IInterface iin = obj.queryLocalInterface(DESCRIPTOR);//查询带有该标识符的Binder对象
+            if (((iin != null) && (iin instanceof IBookManager))) {
+                return (IBookManager) iin;
+            }
+
+            return new BookManagerImpl.Proxy(obj);
+
+        }
 
 
+        @Override
+        protected boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+            switch (code) {
+                case INTERFACE_TRANSACTION: {
+                 reply.writeString(DESCRIPTOR);
+                    return  true;
+                }
+                case TRASACTION_getBookList:{
+                    data.enforceInterface(DESCRIPTOR);
+                    List<Book> result=this.getBookList();
+                    reply.writeNoException();;
+                    reply.writeTypedList(result);
+                    return true;
+                }
+            }
+            return super.onTransact(code, data, reply, flags);
+        }
 
-
-        //Interface中声明的方法
         @Override
         public List<Book> getBookList() throws RemoteException {
+            //TODO 待实现
             return null;
         }
 
         @Override
         public IBinder asBinder() {
+            //TODO 待实现
             return this;
         }
+
+
+        /**
+         * 内部代理类
+         */
+        public static class Proxy implements IBookManager {
+
+            private IBinder mRemote;
+
+            Proxy(IBinder remote) {
+                mRemote = remote;
+            }
+
+            @Override
+            public List<Book> getBookList() throws RemoteException {
+               Parcel data=Parcel.obtain();
+                Parcel reply=Parcel.obtain();
+                List<Book> result;
+                try {
+                    data.writeInterfaceToken(DESCRIPTOR);
+                    mRemote.transact(TRASACTION_getBookList,data,reply,0);
+                    reply.readException();
+                    result=reply.createTypedArrayList(Book.CREATOR);
+                }finally {
+                    reply.recycle();
+                    data.recycle();
+                }
+                
+                return  result;
+            }
+
+            @Override
+            public IBinder asBinder() {
+                return mRemote;
+            }
+
+           public  String getInterfaceDerscriptor(){
+               return  DESCRIPTOR;
+           }
+
+
+
+        }
+
     }
 
 
