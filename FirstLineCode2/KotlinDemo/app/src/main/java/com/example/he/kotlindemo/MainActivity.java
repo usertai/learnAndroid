16b072fcbd407a4ef95442c513ccfff436c47e93
zcczdy <1104503035@qq.com>
2017-05-19 16:24:50 +0800

    commit

---
+++ FirstLineCode2/KotlinDemo/app/src/main/java/com/example/he/kotlindemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.he.kotlindemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
